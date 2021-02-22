eb0fa19a9dbf820cbb52572864fcae039749e347
Alicepinch <alicepinch@hotmail.co.uk>
2021-02-22 12:11:51 +0000

    Profile functionality added

--- app.py
+++ app.py
@@ -104,8 +104,16 @@ def register():
     return render_template("register.html")
 
 
-@app.route('/profile/<username>')
+@app.route('/profile/<username>', methods=["GET", "POST"])
 def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
 
 
 @app.route('/add-recipe')
