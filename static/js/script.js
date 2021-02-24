7e2e018bde6a670e92fb85fe668df6bd77adc830
Alicepinch <alicepinch@hotmail.co.uk>
2021-02-24 20:09:50 +0000

    Modal on click function's added

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,13 @@
-var arrayOfLines = $('textarea').val().split('\n');
+//Setting global variables
+let modal = document.getElementById("delete-modal");
+let btn = document.getElementById("modal-btn");
+let close = document.getElementsByClassName("close")[0];
+
+//Onclick functions to display and hide modal.
+$(btn).click(function(){
+    modal.style.display = "block";
+})
+
+$(close).click(function(){
+    modal.style.display = "none";
+})
