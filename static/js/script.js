0338a9db5b5b38a5463cb102694a3f74bee5f857
Alicepinch <alicepinch@hotmail.co.uk>
2021-03-09 16:48:31 +0000

    Semicolons added after running through JS hint

--- static/js/script.js
+++ static/js/script.js
@@ -1,44 +1,44 @@
 //Setting global variables
 let recipeModal = document.getElementById("delete-recipe-modal");
-let accountModal = document.getElementById("delete-account-modal")
-let profilePicModal = document.getElementById("update-picture-modal")
+let accountModal = document.getElementById("delete-account-modal");
+let profilePicModal = document.getElementById("update-picture-modal");
 let accountBtn = document.getElementsByClassName("account-btn");
 let recipeBtn = document.getElementsByClassName("recipe-btn");
 let profilePicBtn = document.getElementsByClassName("profile-pic-btn");
 let close = document.getElementsByClassName("close");
-let cancel = document.getElementsByClassName("cancel")
+let cancel = document.getElementsByClassName("cancel");
 
 
 //Onclick functions to display and hide modals.
 $(recipeBtn).click(function(){
     recipeModal.style.display = "block";
-})
+});
 
 $(profilePicBtn).click(function(){
     profilePicModal.style.display = "block";
-})
+});
 
 $(accountBtn).click(function(){
     accountModal.style.display = "block";
-})
+});
 
 $(close).click(function(){
     recipeModal.style.display = "none";
     accountModal.style.display = "none";
     profilePicModal.style.display = "none";
-})
+});
 
 $(cancel).click(function(){
     recipeModal.style.display = "none";
     accountModal.style.display = "none";
     profilePicModal.style.display = "none";
-})
+});
 
 // Drop down menu
 
 $(function (){
     $('#recipes_meal_dropdown').hide();
-})
+});
 
 $('#recipeDropDown').click(function () {
     $('#recipes_meal_dropdown').slideToggle();
