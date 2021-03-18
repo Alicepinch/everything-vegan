bbf185e24e97706a2a17998c86c4d12bd1e4abbb
Alicepinch <alicepinch@hotmail.co.uk>
2021-03-18 16:17:50 +0000

    Code clean up and comments improved

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
-# Config #
 
+# Config #
 
 app = Flask(__name__)
 
@@ -35,17 +35,19 @@ meals_data = mongo.db.meals
 
 
 def login_required(f):
+    """
+    Decorator for to be called on views that require users to be logged in.
+    """
+
     @wraps(f)
     def login_check(*args, **kwargs):
         if 'user' not in session:
             return redirect(url_for('login'))
-        else:
-            return f(*args, **kwargs)
+        return f(*args, **kwargs)
     return login_check
 
 
 # Homepage #
-
 
 @app.route('/')
 def index():
@@ -57,28 +59,18 @@ def index():
 @app.route('/recipes/<meal>')
 def meals(meal):
     """
-    Filters different meal types from data.
+    When user clicks on different filter options different
+    meal types will display.
     """
+
     if meal == "breakfast":
-        recipes = recipes_data.find({
-                "$query": {
-                    "meal_name": "Breakfast"
-                    }})
+        recipes = recipes_data.find({"meal_name": "Breakfast"})
     elif meal == "lunch":
-        recipes = recipes_data.find({
-                "$query": {
-                    "meal_name": "Lunch"
-                    }})
+        recipes = recipes_data.find({"meal_name": "Lunch"})
     elif meal == "dinner":
-        recipes = recipes_data.find({
-                "$query": {
-                    "meal_name": "Dinner"
-                    }})
+        recipes = recipes_data.find({"meal_name": "Dinner"})
     elif meal == "desserts":
-        recipes = recipes_data.find({
-                "$query": {
-                    "meal_name": "Desserts"
-                    }})
+        recipes = recipes_data.find({"meal_name": "Desserts"})
 
     return render_template(
             'recipes.html', meal=meal, recipes=recipes)
@@ -97,21 +89,20 @@ def recipes():
 @app.route('/recipes', methods=["GET", "POST"])
 def search():
     """
-    Searches the recipe index.
-    Will return results for, Recipe name,
-    description and ingredients.
-    If not results match - flash message
+    Searches the recipe index. Will return results for, Recipe name,
+    description and ingredients. If not results match - flash message
     will show.
     """
+
     query = request.form.get("search-query")
     all_recipes = recipes_data.find().count()
     recipes = recipes_data.find({"$text": {"$search": query}})
 
     if all_recipes > 0:
         return render_template("recipes.html", recipes=recipes)
-    else:
-        flash("Sorry! No results found 😔")
-        return render_template("recipes.html", recipes=recipes)
+
+    flash("Sorry! No results found 😔")
+    return render_template("recipes.html", recipes=recipes)
 
 
 @app.route('/recipe/<recipe_id>')
@@ -120,83 +111,75 @@ def recipe_page(recipe_id):
     Returns page for
     specific recipe ID.
     """
+
     recipe = recipes_data.find_one({"_id": ObjectId(recipe_id)})
     return render_template('recipe.html', recipe=recipe)
 
 
 # Login / register function #
 
-
 @app.route('/login', methods=["GET", "POST"])
 def login():
     """
-    Log's customer in if username
-    exists and password is correct.
+    Log's customer in if username exists and password is correct.
     """
+
+    if request.method == "GET":
+        return render_template("login.html")
+
     session.permanent = True
     password = request.form.get("password")
 
-    if request.method == "POST":
+    existing_user = users_data.find_one(
+        {"username": request.form.get("username").lower()})
 
-        existing_user = users_data.find_one(
-            {"username": request.form.get("username").lower()})
+    if existing_user and check_password_hash(
+                existing_user["password"], password):
+        session["user"] = request.form.get("username").lower()
 
-        if existing_user:
-            if check_password_hash(
-                    existing_user["password"], password):
-                session["user"] = request.form.get("username").lower()
+        return redirect(url_for(
+                "profile", username=session["user"]))
 
-                return redirect(url_for(
-                    "profile", username=session["user"]))
-
-            else:
-                flash("Incorret Username and/or Password")
-                return redirect(url_for("login"))
-
-        else:
-            flash("Incorrect Username and/or Password")
-            return redirect(url_for("login"))
-
-    return render_template("login.html")
+    flash("Incorrect Username and/or Password")
+    return redirect(url_for("login"))
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     """
-    Registers new user if
-    username/email is not used
-    and add's data to mongo db.
+    Registers new user if username/email is not
+    used and add's data to mongo db.
     """
-    if request.method == "POST":
 
-        existing_username = users_data.find_one(
-            {"username": request.form.get("username").lower()})
+    if request.method == "GET":
+        return render_template("register.html")
+
+    existing_username = users_data.find_one(
+        {"username": request.form.get("username").lower()})
+    existing_email = users_data.find_one(
+        {"email": request.form.get("email").lower()})
+
+    if existing_username or existing_email:
         if existing_username:
-            flash("Sorry, this username already exists. Please try another")
-            return redirect(url_for("register"))
+            flash("Sorry, this username is already in use. Please try another")
+        else:
+            flash("Sorry, this email is already in use. Please try another")
+        return redirect(url_for("register"))
 
-        existing_email = users_data.find_one(
-            {"email": request.form.get("email").lower()})
-        if existing_email:
-            flash("Sorry, this email is in use. Please try another")
-            return redirect(url_for("register"))
+    register = {
+        "username": request.form.get("username").lower(),
+        "email": request.form.get("email").lower(),
+        "password": generate_password_hash(request.form.get("password")),
+        "date_joined": date.strftime("%d/%m/%Y"),
+        "profile_image": request.form.get(
+                                "profile_img") or default_pic
+    }
+    users_data.insert_one(register)
 
-        register = {
-            "username": request.form.get("username").lower(),
-            "email": request.form.get("email").lower(),
-            "password": generate_password_hash(request.form.get("password")),
-            "date_joined": date.strftime("%d/%m/%Y"),
-            "profile_image": request.form.get(
-                                 "profile_img") or default_pic
-        }
-        users_data.insert_one(register)
-
-        session["user"] = request.form.get("username").lower()
-        flash("Welcome! Thank you for sigining up!😊")
-        return redirect(url_for(
-            "profile", username=session["user"]))
-
-    return render_template("register.html")
+    session["user"] = request.form.get("username").lower()
+    flash("Welcome! Thank you for sigining up!😊")
+    return redirect(url_for(
+        "profile", username=session["user"]))
 
 
 @app.route('/logout')
@@ -205,6 +188,7 @@ def logout():
     """
     Logs user out from session.
     """
+
     flash("Goodbye! You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
@@ -212,21 +196,20 @@ def logout():
 
 # User logged in functions #
 
-
 @app.route('/profile/<username>', methods=["GET", "POST"])
 @login_required
 def profile(username):
     """
-    If the user has added recipes then
-    they will display on profile page.
+    Profile will display all recipes created by user.
+    If admin is logged in they can view all recipes on profile.
     """
+
     user = users_data.find_one({"username": session['user']})
 
     if session['user'] == "admin":
         recipes = list(recipes_data.find())
-    else:
-        recipes = list(recipes_data.find(
-                {"created_by": session['user']}))
+
+    recipes = list(recipes_data.find({"created_by": session['user']}))
     return render_template(
         "profile.html", user=user, recipes=recipes, username=username)
 
@@ -238,28 +221,30 @@ def add_recipe():
     Add recipe function allows the
     user to add their own recipes if logged in
     """
-    if request.method == "POST":
-        recipe = {
-            "meal_name": request.form.get("meal_name"),
-            "recipe_name": request.form.get("recipe_name"),
-            "ingredients": request.form.get("ingredients"),
-            "description": request.form.get("description").capitalize(),
-            "recommendation": request.form.get("recos").capitalize(),
-            "yield": request.form.get("yield"),
-            "active_time": request.form.get(
-                "active_time").replace('mins', 'minutes').title(),
-            "total_time": request.form.get("total_time"),
-            "img_url": request.form.get("img_url") or default_img,
-            "method": request.form.get("method"),
-            "created_by": session["user"],
-            "date_created": date.strftime("%d/%m/%Y")
-        }
 
-        recipes_data.insert_one(recipe)
-        flash("Recipe Succesfully Added")
-        return redirect(url_for("recipes"))
+    if request.method == "GET":
+        return render_template('add-recipe.html')
 
-    return render_template('add-recipe.html')
+    recipe = {
+        "meal_name": request.form.get("meal_name"),
+        "recipe_name": request.form.get("recipe_name"),
+        "ingredients": request.form.get("ingredients"),
+        "description": request.form.get("description").capitalize(),
+        "recommendation": request.form.get("recos").capitalize(),
+        "yield": request.form.get("yield"),
+        "active_time": request.form.get(
+            "active_time").replace('mins', 'minutes').title(),
+        "total_time": request.form.get(
+            "total_time").replace('mins', 'minutes').capitalize(),
+        "img_url": request.form.get("img_url") or default_img,
+        "method": request.form.get("method"),
+        "created_by": session["user"],
+        "date_created": date.strftime("%d/%m/%Y")
+    }
+
+    recipes_data.insert_one(recipe)
+    flash("Recipe Succesfully Added")
+    return redirect(url_for("recipes"))
 
 
 @app.route('/edit-recipe/<recipe_id>', methods=["GET", "POST"])
@@ -270,31 +255,31 @@ def edit_recipe(recipe_id):
     user to edit their own recipes from
     their profile page.
     """
+
     recipe = recipes_data.find_one({"_id": ObjectId(recipe_id)})
 
-    if request.method == "POST":
-        recipes_data.update_one(
-            {"_id": ObjectId(recipe_id)},
-            {'$set': {
-                "meal_name": request.form.get("meal_name"),
-                "recipe_name": request.form.get("recipe_name"),
-                "ingredients": request.form.get("ingredients"),
-                "description": request.form.get("description").capitalize(),
-                "recommendation": request.form.get("recos").capitalize(),
-                "yield": request.form.get("yield"),
-                "active_time": request.form.get(
-                    "active_time").replace('mins', 'minutes').title(),
-                "total_time": request.form.get(
-                    "total_time").title().replace('mins', 'minutes'),
-                "img_url": request.form.get("img_url") or default_img,
-                "method": request.form.get("method"),
-                "created_by": session["user"]
-            }})
+    if request.method == "GET":
+        return render_template('edit-recipe.html', recipe=recipe)
 
-        flash("Recipe Updated 😊")
-        return redirect(url_for("recipe_page", recipe_id=recipe_id))
+    recipes_data.update_one(
+        {"_id": ObjectId(recipe_id)},
+        {'$set': {
+            "meal_name": request.form.get("meal_name"),
+            "recipe_name": request.form.get("recipe_name"),
+            "ingredients": request.form.get("ingredients"),
+            "description": request.form.get("description").capitalize(),
+            "recommendation": request.form.get("recos").capitalize(),
+            "yield": request.form.get("yield"),
+            "active_time": request.form.get(
+                "active_time").replace('mins', 'minutes').title(),
+            "total_time": request.form.get(
+                "total_time").replace('mins', 'minutes').capitalize(),
+            "img_url": request.form.get("img_url") or default_img,
+            "method": request.form.get("method")
+        }})
 
-    return render_template('edit-recipe.html', recipe=recipe)
+    flash("Recipe Updated 😊")
+    return redirect(url_for("recipe_page", recipe_id=recipe_id))
 
 
 @app.route('/delete-recipe/<recipe_id>')
@@ -303,6 +288,7 @@ def delete_recipe(recipe_id):
     """
     Removes recipe from database and recipes page.
     """
+
     recipes_data.delete_one({"_id": ObjectId(recipe_id)})
     flash("Recipe Succesfully Removed!")
     return redirect(url_for("recipes"))
@@ -310,18 +296,18 @@ def delete_recipe(recipe_id):
 
 # Update/ delete users #
 
-
 @app.route('/delete-account/<username>')
 @login_required
 def delete_user(username):
     """
-    Ends user session.
-    Removes user & all
-    recipes created by user.
+    If the session user is the username that is logged in or
+    the admin then they can delete their account with this URL.
     """
-    users_data.remove({"username": session['user']})
-    session.pop("user")
-    recipes_data.remove({"created_by": username})
+
+    if session['user'] == username or "admin":
+        users_data.remove({"username": session['user']})
+        session.pop("user")
+        recipes_data.remove({"created_by": username})
 
     flash("Sorry to see you go! Your user has been deleted.")
     return redirect(url_for("login"))
@@ -335,34 +321,32 @@ def update_user(username):
     Updates password if the two new passwords match.
     If passwords dont match - flash message appears.
     """
+
     current_password = request.form.get("password")
     new_password = request.form.get('new-password')
     confirm_password = request.form.get("confirm-password")
     users = users_data
     user = users_data.find_one({'username': session['user']})
 
-    if request.method == "POST":
+    if request.method == "GET":
+        return render_template('update-user.html', username=username)
 
-        if check_password_hash(user["password"], current_password):
+    if check_password_hash(user["password"], current_password):
+        if new_password == confirm_password:
+            users.update_one(
+                {'username': session['user']},
+                {'$set': {
+                    'password': generate_password_hash
+                    (new_password)
+                }})
+            flash("Password updated! 😊")
+            return redirect(url_for('profile', username=username))
 
-            if new_password == confirm_password:
-                users.update_one(
-                    {'username': session['user']},
-                    {'$set': {
-                        'password': generate_password_hash
-                        (new_password)
-                    }})
-                flash("Password updated! 😊")
-                return redirect(url_for('profile', username=username))
+        flash("Passwords do not match! Please try again😔")
+        return redirect(url_for("update_user", username=username))
 
-            else:
-                flash("Passwords do not match! Please try again😔")
-                return redirect(url_for("update_user", username=username))
-        else:
-            flash('Incorrect password. Please try again😔')
-            return redirect(url_for('update_user', username=username))
-
-    return render_template('update-user.html', username=username)
+    flash('Incorrect password. Please try again😔')
+    return redirect(url_for('update_user', username=username))
 
 
 @app.route('/update-profile-pic/<username>', methods=["GET", "POST"])
@@ -371,17 +355,19 @@ def update_profile_pic(username):
     """
     Updates users profile photo.
     """
-    users_data.update_one(
-        {"username": session['user']},
-        {'$set': {
-            "profile_image": request.form.get(
-                                 "profile_img")
-            }})
+
+    if session['user'] == username:
+        users_data.update_one(
+            {"username": session['user']},
+            {'$set': {
+                "profile_image": request.form.get(
+                                    "profile_img")
+                }})
+
     return redirect(url_for("profile", username=username))
 
 
 # Newsletter Subscribe #
-
 
 @app.route('/subscribe', methods=["GET", "POST"])
 def subscribe_user():
@@ -389,6 +375,7 @@ def subscribe_user():
     First checks if email is subscribed already.
     If email is not subscribed, email is added to database.
     """
+
     existing_sub = subscribers_data.find_one(
             {"subscriber_email": request.form.get("sub_email")})
 
@@ -401,7 +388,7 @@ def subscribe_user():
         }
     subscribers_data.insert_one(subscribe)
     flash("Thank you for subscribing!")
-    return redirect(request.referrer + "#subscribe")
+    return redirect(request.referrer)
 
 
 # Error Pages #
